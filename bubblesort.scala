37dd73a32162c2822befac4d734317058d9ed907
RISHI PATWA <rishi17p@gmail.com>
2026-05-04 14:46:48 +0530

    Refactor bubblesort function to sort method

--- bubblesort.scala
+++ bubblesort.scala
@@ -1,21 +1,17 @@
-def bubblesort(arr: Array[Int]) {
-    val n = arr.length
-
-    for (i <- 0 until n) {
-        for (j <- 0 until n - 1) {
-            if (arr(j) > arr(j + 1)) {
-                val temp = arr(j)
-                arr(j) = arr(j + 1)
-                arr(j + 1) = temp
-            }
+def sort(arr:Array[Int]){
+        val n=arr.length
+        for(i<-0 until n){
+        for(j<-0 until n-1){
+        if(arr(j)>arr(j+1)){
+        val temp=arr(j)
+        arr(j)=arr(j+1)
+        arr(j+1)=temp
         }
-    }
-
-    print("Sorted Array: ")
-
-    arr.foreach(x => print(x + " "))
+        }
+        }
+        println("Sorted Array: ")
+        arr.foreach(println)
 }
 
-val arr = Array(5, 3, 1, 2, 4)
-
-bubblesort(arr)
+val arr=Array(5,3,1,2,4)
+sort(arr)
